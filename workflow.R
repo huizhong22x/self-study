1d86a14e954c3a4792c9d180a34037af14f1b3ab
huizhong22x <huizhong22x@gmail.com>
2024-09-04 21:05:11 -0400

    text commit

---
+++ workflow.R
@@ -0,0 +1,12 @@
+# hw0
+install.packages(
+  c("tidyverse", "knitr", "rmarkdown", "janitor", "broom", "here", "readxl", 
+    "haven", "rnoaa", "ggridges", "ggthemes", "leaflet", "viridis", "skimr", 
+    "rvest", "httr", "flexdashboard", "devtools", "usethis", "modelr", "mgcv", 
+    "tidytext", "shiny", "patchwork", "glmnet", "plotly"))
+
+remotes::install_github("p8105/p8105.datasets")
+remotes::install_github("ropensci/rnoaa")
+
+# set ssh
+# set personal access token
