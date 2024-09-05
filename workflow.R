5d9c0a08c135871d55bb339654c7ab7bafb7b79c
huizhong22x <huizhong22x@gmail.com>
2024-09-04 22:40:06 -0400

    test

--- workflow.R
+++ workflow.R
@@ -1,15 +1,17 @@
 # hw0
-install.packages(
-  c("tidyverse", "knitr", "rmarkdown", "janitor", "broom", "here", "readxl", 
-    "haven", "rnoaa", "ggridges", "ggthemes", "leaflet", "viridis", "skimr", 
-    "rvest", "httr", "flexdashboard", "devtools", "usethis", "modelr", "mgcv", 
-    "tidytext", "shiny", "patchwork", "glmnet", "plotly"))
-
-remotes::install_github("p8105/p8105.datasets")
-remotes::install_github("ropensci/rnoaa")
+# install.packages(
+#   c("tidyverse", "knitr", "rmarkdown", "janitor", "broom", "here", "readxl", 
+#     "haven", "rnoaa", "ggridges", "ggthemes", "leaflet", "viridis", "skimr", 
+#     "rvest", "httr", "flexdashboard", "devtools", "usethis", "modelr", "mgcv", 
+#     "tidytext", "shiny", "patchwork", "glmnet", "plotly"))
+# 
+# remotes::install_github("p8105/p8105.datasets")
+# remotes::install_github("ropensci/rnoaa")
 
 # set ssh
 # set personal access token
+# Terminal
 # git config -m "text commit"
 # git config --global user.email ""
 # git config --global user.name ""
+
